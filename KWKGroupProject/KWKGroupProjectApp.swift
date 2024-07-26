a850a6c8a1653ecca0381a86d665f0919482d0ad
Shreenidhi Kunta <shreenidhi.kunta@gmail.com>
2024-07-26 12:02:52 -0500

    Initial Commit

---
+++ KWKGroupProject/KWKGroupProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWKGroupProjectApp.swift
+//  KWKGroupProject
+//
+//  Created by Shreenidhi Kunta on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct KWKGroupProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
