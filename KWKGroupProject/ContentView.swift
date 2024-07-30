e44e566be4317f45a69156eaf1422441ea2505eb
Shreenidhi Kunta <shreenidhi.kunta@gmail.com>
2024-07-30 10:58:58 -0500

    added a second view

--- KWKGroupProject/ContentView.swift
+++ KWKGroupProject/ContentView.swift
@@ -8,56 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
     @State private var firstName = ""
     @State private var lastName = ""
     var body: some View {
-        ZStack{
-            Color.teal.opacity(0.8)
-                .ignoresSafeArea()
-            VStack (alignment: .leading, spacing: 22.0){
-                Text("Create new account")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .foregroundStyle(.white)
-                
-                Text(" First Name:")
-                    .foregroundStyle(.white)
-                    .multilineTextAlignment(.leading)
-                TextField("", text: $firstName)
-                    .multilineTextAlignment(.center)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .border(Color.gray, width : 1)
-                    .foregroundStyle(.white)
-                
-                Text(" Last Name:")
-                    .foregroundStyle(.white)
-                TextField("", text: $lastName)
-                    .multilineTextAlignment(.center)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .border(Color.gray, width : 1)
-                    .foregroundStyle(.white)
-                
-                Text(" Email:")
-                    .foregroundStyle(.white)
-                TextField("", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                    .multilineTextAlignment(.center)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .border(Color.gray, width : 1)
-                    .foregroundStyle(.white)
-                    .autocorrectionDisabled()
-                    .textInputAutocapitalization(.never)
-                
-                Text(" Password:")
-                    .foregroundStyle(.white)
-                TextField("", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                    .multilineTextAlignment(.center)
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .border(Color.gray, width : 1)
-                    .foregroundStyle(.white)
-                    .textInputAutocapitalization(.never)
+        NavigationStack {
+            ZStack{
+                Color.teal.opacity(0.8)
+                    .ignoresSafeArea()
+                VStack (alignment: .leading, spacing: 22.0){
+                    Text("Create new account")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(.white)
+                    
+                    Text(" First Name:")
+                        .foregroundStyle(.white)
+                        .multilineTextAlignment(.leading)
+                    TextField("", text: $firstName)
+                        .multilineTextAlignment(.center)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .border(Color.gray, width : 1)
+                        .foregroundStyle(.white)
+                    
+                    Text(" Last Name:")
+                        .foregroundStyle(.white)
+                    TextField("", text: $lastName)
+                        .multilineTextAlignment(.center)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .border(Color.gray, width : 1)
+                        .foregroundStyle(.white)
+                    
+                    Text(" Email:")
+                        .foregroundStyle(.white)
+                    TextField("", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .multilineTextAlignment(.center)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .border(Color.gray, width : 1)
+                        .foregroundStyle(.white)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                    
+                    Text(" Password:")
+                        .foregroundStyle(.white)
+                    TextField("", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .multilineTextAlignment(.center)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .border(Color.gray, width : 1)
+                        .foregroundStyle(.white)
+                        .textInputAutocapitalization(.never)
+                    
+                    
+                    
+                    NavigationLink(destination: SecondView())
+                    {
+                        Text("Take me to a new view")
+                    }
+                    .navigationTitle("Home")
+                    .navigationBarHidden(true)
+                    
+                }
                 
             }
         }
+        
     }
 }
 
---
+++ KWKGroupProject/SecondView.swift
@@ -0,0 +1,25 @@
+//
+//  SecondView.swift
+//  KWKGroupProject
+//
+//  Created by Shreenidhi Kunta on 7/30/24.
+//
+
+
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        ZStack{
+            Color.teal.opacity(0.8)
+                //.ignoresSafeArea()
+            Text("Hello User")
+            
+        }
+    }
+}
+
+#Preview {
+    SecondView()
+}
