508ebf2972fe9d903f87e8293cdc2e67fa27b92e
Shreenidhi Kunta <shreenidhi.kunta@gmail.com>
2024-08-01 08:05:12 -0500

    Made functional category buttons

--- ThirdView.swift
+++ ThirdView.swift
@@ -11,6 +11,9 @@ struct ThirdView: View {
     @State private var category1 = ""
     @State private var category2 = ""
     @State private var category3 = ""
+    @State private var category1On = false
+    @State private var category2On = false
+    @State private var category3On = false
     
     
     
@@ -20,13 +23,72 @@ struct ThirdView: View {
                 .ignoresSafeArea()
             VStack(spacing: 20.0){
                 Text("Budgeting Map")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                    .foregroundStyle(.white)
                 Text("Plan your budget for the week")
-                Button("Add a category")
-                {
-                    TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $category1)
+                    .font(.headline)
+                    .bold()
+                    .foregroundStyle(.white)
+                    .padding()
+                
+               
+        
+                
+                VStack(spacing: 20.0) {
+                    if (!category1On)
+                    {
+                        Button("+ Add a category")
+                        {
+                            category1On = true
+                        }
+                        .opacity(!category1On ? 1: 0)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.orange)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                    }
+                    else
+                    {
+                        TextField("Enter value here", text: $category1)
+                            .multilineTextAlignment(.center)
+                    }
+                    if (!category2On)
+                    {
+                        Button("+ Add a category")
+                        {
+                            category2On = true
+                        }
+                        .opacity(!category2On ? 1: 0)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.orange)
+                        .padding()
+                    }
+                    
+                    else
+                    {
+                        TextField("Enter value here", text: $category2)
+                            .multilineTextAlignment(.center)
+                    }
+                    if (!category3On)
+                    {
+                        Button("+ Add a category")
+                        {
+                            category3On = true
+                        }
+                        .opacity(!category3On ? 1: 0)
+                        .buttonStyle(.borderedProminent)
+                        .tint(.orange)
+                        .padding()
+                    }
+                    else
+                    {
+                        TextField("Enter value here", text: $category3)
+                            .multilineTextAlignment(.center)
+                    }
+                    
                 }
-                .buttonStyle(.borderedProminent)
-                .tint(.orange)
+            
 
                
                
