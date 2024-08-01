a8e8d778d3983efc2bee358b086ab8101b6bb3e4
Shreenidhi Kunta <shreenidhi.kunta@gmail.com>
2024-08-01 09:44:06 -0500

    Added more functionality to button

---
+++ FourthView.swift
@@ -0,0 +1,18 @@
+//
+//  FourthView.swift
+//  KWKGroupProject
+//
+//  Created by Shreenidhi Kunta on 8/1/24.
+//
+
+import SwiftUI
+
+struct FourthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FourthView()
+}
